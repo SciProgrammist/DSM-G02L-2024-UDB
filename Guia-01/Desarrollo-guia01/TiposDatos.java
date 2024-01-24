b848d6ab166e57e4e379c61518fd6af3ddcd0a4e
ReyesWorks <RP191495@alumno.udb.edu.sv>
2024-01-24 08:51:23 -0600

    eje01

---
+++ Guia-01/Desarrollo-guia01/TiposDatos.java
@@ -0,0 +1,19 @@
+
+/*
+    Asignatura Java Avanzada
+    Programa de ejemplo de datos en Java.
+    API: Es un conjunto de funciones y clases.
+*/
+
+public class TiposDatos{
+    public static void main(String[] args)
+    {
+        int var01 = 2000;
+        long var02 = 10000;
+        float var03 = 6.5F;
+        double var04 = 10.7;
+        char var05 = '\u0022'; // Se asigna a var05 un valor constante Unicode
+        char var06 = 'k';
+
+    }
+}
